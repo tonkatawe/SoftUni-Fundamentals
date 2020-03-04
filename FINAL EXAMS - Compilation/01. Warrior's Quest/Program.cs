bc4a0f2b861ab6581397d9433a66e46128dfea37
Anton <vlahov.anton@gmail.com>
2020-03-04 17:10:52 +0200

    C# Fundamentals

---
+++ FINAL EXAMS - Compilation/01. Warrior's Quest/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01._Warrior_s_Quest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            string result = command;
+
+            while (command != "For Azeroth")
+            {
+                var tokens = command.Split(" ");
+                var text = tokens[0];
+                if (text == "GladiatorStance")
+                {
+                    Console.WriteLine(text.ToUpper());
+                }
+                else if (text == "DefensiveStance")
+                {
+                    Console.WriteLine(text.ToLower());
+                }
+                else if (text == "Dispel")
+                {
+                    var index = int.Parse(tokens[1]);
+                    char letter = char.Parse(tokens[2]);
+                    if (index <= result.Length)
+                    {
+                        result = result.Replace(result[index], letter);
+                        Console.WriteLine("Success!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dispel too weak.");
+                    }
+                }
+                else if (text == "Target")
+                {
+                    if (tokens[1] == "Change") // might be have to check contains
+                    {
+                        var substring = tokens[2];
+                        var substringNew = tokens[3];
+                        result = result.Replace(substring, substringNew);
+                        Console.WriteLine(result);
+
+                    }
+                    else if (tokens[1] == "Remove")
+                    {
+                        var substring = tokens[2];
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Command doesn't exist!");
+                }
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ FINAL EXAMS - Compilation/03. Hero Recruitment/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Hero_Recruitment
+{
+    // check why 90/100
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> heroes = new Dictionary<string, List<string>>();
+            var command = Console.ReadLine();
+            while (command != "End")
+            {
+                var tokens = command.Split();
+                var instruction = tokens[0];
+                if (instruction == "Enroll")
+                {
+                    var heroName = tokens[1];
+                    if (!heroes.ContainsKey(heroName))
+                    {
+                        heroes.Add(heroName, new List<string>());
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    Console.WriteLine($"{heroName} is already enrolled.");
+                }
+                else if (instruction == "Learn")
+                {
+                    var heroName = tokens[1];
+                    var heroSpell = tokens[2];
+                    if (!heroes.ContainsKey(heroName))
+                    {
+                        Console.WriteLine($"{heroName} doesn't exist.");
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                   else if (heroes.ContainsKey(heroName) && !heroes[heroName].Contains(heroSpell))
+                    {
+                        heroes[heroName].Add(heroSpell);
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    else if (heroes.ContainsKey(heroName) && heroes[heroName].Contains(heroSpell))
+                    {
+                        Console.WriteLine($"{heroName} has already learnt {heroSpell}.");
+                    }
+                }
+                else if (instruction == "Unlearn")
+                {
+                    var heroName = tokens[1];
+                    var heroSpell = tokens[2];
+                    if (!heroes.ContainsKey(heroName))
+                    {
+                        Console.WriteLine($"{heroName} doesn't exist.");
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    else if (heroes.ContainsKey(heroName) && heroes[heroName].Contains(heroSpell))
+                    {
+                        heroes[heroName].Remove(heroSpell);
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                    else if (heroes.ContainsKey(heroName) && !heroes[heroName].Contains(heroSpell))
+                    {
+                        Console.WriteLine($"{heroName} doesn't know {heroSpell}.");
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Heroes:");
+            foreach (var hero in heroes.OrderBy(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"== {hero.Key}: {string.Join(", ", hero.Value)}");
+            }
+        }
+    }
+}
