440dab9385d8c91c8f1e2eeffce48d35e46bc67d
tonkatawe <vlahov.anton@gmail.com>
2020-03-10 00:03:55 +0200

    C# Fundamentals

--- FINAL EXAMS - Compilation/Username/Program.cs
+++ FINAL EXAMS - Compilation/Username/Program.cs
@@ -31,12 +31,10 @@ namespace Username
                 {
                     var startIndex = int.Parse(tokens[1]);
                     var endIndex = int.Parse(tokens[2]);
-                    if (startIndex >=0 && endIndex < username.Length && endIndex > startIndex)
+                    if (startIndex >= 0 && endIndex < username.Length && endIndex > startIndex)
                     {
-                        var temp = username.Substring(startIndex, endIndex); //here might use loop
-                        var newtemp = temp.ToCharArray().Reverse();
-                      //  Array.Reverse(newtemp);
-                        Console.WriteLine(string.Join("",newtemp));
+                        var temp = username.Substring(startIndex, (endIndex - startIndex) + 1); 
+                        Console.WriteLine(string.Join("", temp.Reverse()));
                     }
 
                 }
@@ -45,13 +43,39 @@ namespace Username
                     var substring = tokens[1];
                     if (username.Contains(substring))
                     {
-                      //to be continue;
-
+                        var startIndex = username.IndexOf(substring);
+                        username = username.Remove(startIndex, substring.Length);
+                        Console.WriteLine(username);
                     }
                     else
                     {
                         Console.WriteLine($"The word {username} doesn't contain {substring}.");
                     }
+                }
+                else if (tokens[0] == "Replace")
+                {
+                    var oldChar = char.Parse(tokens[1]);
+                    if (username.Contains(oldChar))
+                    {
+                        username = username.Replace(oldChar, '*');
+                        Console.WriteLine(username);
+                    }
+                }
+                else if (tokens[0] == "Check")
+                {
+                    var oldChar = char.Parse(tokens[1]);
+                    if (username.Contains(oldChar))
+                    {
+                        Console.WriteLine("Valid");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Your username must contain {oldChar}.");
+                    }
+                }
+                else if (command == "Sign up")
+                {
+                    break;
                 }
 
             }
