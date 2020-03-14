d93606b4f45906fa2858406bb5719197c4fecd1a
tonkatawe <vlahov.anton@gmail.com>
2020-03-14 21:23:43 +0200

    C# Fundamentals

--- FINAL EXAMS - Compilation/02. Message Encrypter/Program.cs
+++ FINAL EXAMS - Compilation/02. Message Encrypter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
 
 namespace _02._Message_Encrypter
 {
@@ -6,7 +8,26 @@ namespace _02._Message_Encrypter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = int.Parse(Console.ReadLine()); // number of commands
+            string pattern = @"(?<name>\D)([A-Z][a-z]{2,})\k'name': \[?([A-z])\]\|\[?([A-z])\]\|\[?([A-z])\]\|$";
+            var regex = new Regex(pattern);
+            for (int i = 0; i < n; i++)
+            {
+                var someText = Console.ReadLine();
+                var match = regex.Match(someText);
+                if (match.Success)
+                {
+                    var firstNumber = (int)Convert.ToChar(match.Groups[2].Value);
+                    var secondNumber = (int)Convert.ToChar(match.Groups[3].Value);
+                    var thirdNumber = (int)Convert.ToChar(match.Groups[4].Value);
+                    Console.WriteLine($"{match.Groups[1].Value}: {firstNumber} {secondNumber} {thirdNumber}");
+                }
+                else
+                {
+                    Console.WriteLine("Valid message not found!");
+                }
+            }
+
         }
     }
 }
---
+++ FINAL EXAMS - Compilation/03. Battle Manager/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Battle_Manager
+{
+    class Program
+    {
+        // check 91/100
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> userAndHealth = new Dictionary<string, int>();
+            Dictionary<string, int> userAndEnergy = new Dictionary<string, int>();
+            while (true)
+            {
+                var tokens = Console.ReadLine().Split(':');
+                var command = tokens[0];
+                if (command == "Add")
+                {
+                    var username = tokens[1];
+                    var health = int.Parse(tokens[2]);
+                    var energy = int.Parse(tokens[3]);
+                    if (health > 0 && energy > 0)
+                    {
+                        if (!userAndHealth.ContainsKey(username))
+                        {
+                            userAndHealth.Add(username, health);
+                            userAndEnergy.Add(username, energy);
+                        }
+                        else
+                        {
+                            userAndHealth[username] += health;
+                            userAndEnergy[username] += energy;
+                        }
+                    }
+                }
+                else if (command == "Attack")
+                {
+                    var attackerName = tokens[1];
+                    var defenderName = tokens[2];
+                    var damage = int.Parse(tokens[3]);
+                    if (userAndHealth.ContainsKey(attackerName) && userAndHealth.ContainsKey(defenderName) && damage > 0)
+                    {
+                        userAndHealth[defenderName] -= damage;
+                        userAndEnergy[attackerName] -= 1;
+                        if (userAndHealth[defenderName] <= 0)
+                        {
+                            Console.WriteLine($"{defenderName} was disqualified!");
+                            userAndEnergy.Remove(defenderName);
+                            userAndHealth.Remove(defenderName);
+                        }
+                        if (userAndEnergy[attackerName] == 0) //here might be need to chek with <=
+                        {
+                            Console.WriteLine($"{attackerName} was disqualified!");
+                            userAndEnergy.Remove(attackerName);
+                            userAndHealth.Remove(attackerName);
+                        }
+                    }
+                }
+                else if (command == "Delete")
+                {
+                    var username = tokens[1];
+                    if (userAndHealth.ContainsKey(username))
+                    {
+                        userAndHealth.Remove(username);
+                        userAndEnergy.Remove(username);
+                    }
+                    else if (username == "All")
+                    {
+                        userAndEnergy.Clear();
+                        userAndHealth.Clear();
+                    }
+                }
+                else if (command == "Results")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"People count: {userAndHealth.Count}");
+            foreach (var user in userAndHealth.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{user.Key} - {user.Value} - {userAndEnergy[user.Key]}");
+            }
+        }
+    }
+}
