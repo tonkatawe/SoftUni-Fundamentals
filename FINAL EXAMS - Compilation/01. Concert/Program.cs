cd358459d2759d78ae36ccb135269de1700b4724
tonkatawe <vlahov.anton@gmail.com>
2020-03-18 23:55:08 +0200

    C# Fundamentals

---
+++ FINAL EXAMS - Compilation/01. Concert/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+
+namespace _01._Concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> bandAndMembers = new Dictionary<string, List<string>>();
+            Dictionary<string, int> timeAndBand = new Dictionary<string, int>();
+            var timeSum = 0;
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "start of concert")
+                {
+                    break;
+                }
+
+                var input = command.Split("; ");
+                var instruction = input[0];
+                var bandName = input[1];
+                if (instruction == "Add")
+                {
+                    var members = input[2].Split(", ").ToList();
+                    if (!bandAndMembers.ContainsKey(bandName))
+                    {
+                        bandAndMembers.Add(bandName, new List<string>(members));
+                        timeAndBand.Add(bandName, 0);
+                    }
+                    else
+                    {
+                        foreach (var member in members)
+                        {
+                            if (!bandAndMembers[bandName].Contains(member))
+                            {
+                                bandAndMembers[bandName].Add(member);
+                            }
+                        }
+                    }
+                }
+                else if (instruction == "Play")
+                {
+                    var time = int.Parse(input[2]);
+                    if (!timeAndBand.ContainsKey(bandName) && time > 0)
+                    {
+                        timeAndBand.Add(bandName, time);
+                        bandAndMembers.Add(bandName, new List<string>());
+                        timeSum += time;
+                    }
+                    else if (time > 0)
+                    {
+                        timeAndBand[bandName] += time;
+                        timeSum += time;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Total time: {timeSum}");
+            foreach (var band in timeAndBand.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{band.Key} -> {band.Value}");
+            }
+
+            var someBand = Console.ReadLine();
+
+            Console.WriteLine(someBand);
+            foreach (var member in bandAndMembers[someBand])
+            {
+                Console.WriteLine($"=> {member}");
+            }
+
+        }
+    }
+}
---
+++ FINAL EXAMS - Compilation/03. Messages Manager v2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Messages_Manager_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var capacity = int.Parse(Console.ReadLine());
+            Dictionary<string, int> userSent = new Dictionary<string, int>();
+            Dictionary<string, int> userReceived = new Dictionary<string, int>();
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "Statistics")
+                {
+                    break;
+                }
+                var tokens = command.Split('=');
+                var instruction = tokens[0];
+                if (instruction == "Add")
+                {
+                    var username = tokens[1];
+                    var sent = int.Parse(tokens[2]);
+                    var received = int.Parse(tokens[3]);
+                    if (!userSent.ContainsKey(username) && sent >= 0 && received >= 0)
+                    {
+                        userSent.Add(username, sent);
+                        userReceived.Add(username, received);
+                    }
+                }
+                else if (instruction == "Message")
+                {
+                    var sender = tokens[1];
+                    var receiver = tokens[2];
+                    if (userSent.ContainsKey(sender) && userReceived.ContainsKey(receiver))
+                    {
+                        userSent[sender] += 1;
+                        userReceived[receiver] += 1;
+                        if (userSent[sender] + userReceived[sender] == capacity)
+                        {
+                            Console.WriteLine($"{sender} reached the capacity!");
+                            userSent.Remove(sender);
+                            userReceived.Remove(sender);
+
+                        }
+
+                        if (userSent[receiver] + userReceived[receiver] == capacity)
+                        {
+                            Console.WriteLine($"{receiver} reached the capacity!");
+                            userSent.Remove(receiver);
+                            userReceived.Remove(receiver);
+                        }
+                    }
+
+
+                }
+                else if (instruction == "Empty")
+                {
+                    var username = tokens[1];
+                    if (userSent.ContainsKey(username) && userReceived.ContainsKey(username))
+                    {
+                        userSent.Remove(username);
+                        userReceived.Remove(username);
+                    }
+                    else if (username == "All")
+                    {
+                        userSent.Clear();
+                        userReceived.Clear();
+                    }
+                }
+
+            }
+            Console.WriteLine($"Users count: {userSent.Count}");
+            foreach (var user in userReceived.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{user.Key} - {userSent[user.Key] + userReceived[user.Key]}");
+            }
+        }
+    }
+}
