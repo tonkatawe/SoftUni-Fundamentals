0a82a1f1d0b57944ea8854d8caa2dcd99d72b869
tonkatawe <vlahov.anton@gmail.com>
2020-03-19 15:57:39 +0200

    C# Fundamentals

---
+++ FINAL EXAMS - Compilation/02. Song Encryption/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _02._Song_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+
+                var tokens = command.Split(':');
+                var artist = tokens[0].ToCharArray();
+                var song = tokens[1].ToCharArray();
+                if (CheckArtistIsAvaible(artist) && CheckSongIsAvaible(song))
+                {
+                    var key = artist.Length;
+                    Console.WriteLine($"Successful encryption: {EncryptArtist(artist, key)}@{EncryptArtist(song, key)}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+        }
+
+        static string EncryptArtist(char[] artist, int key)
+        {
+
+            var encryptedArtist = string.Empty;
+            for (int i = 0; i < artist.Length; i++)
+            {
+                if (artist[i] == ' ' || artist[i] == '\'')
+                {
+                    encryptedArtist += artist[i];
+                    continue;
+                }
+                if (artist[i] + (char)key > 'z')
+                {
+                    var tempKey = (int)(artist[i]) + key - 26;
+                    artist[i] = (char)(tempKey);
+                    encryptedArtist += artist[i];
+                    continue;
+                }
+
+                if (artist[i] + (char)key > 'Z' && char.IsUpper(artist[i]))
+                {
+                    int tempKey = (int)(artist[i]) + key - 26;
+                    artist[i] = (char)(tempKey);
+                    encryptedArtist += artist[i];
+
+                    continue;
+
+                }
+                artist[i] += (char)key;
+                encryptedArtist += artist[i];
+            }
+            return encryptedArtist;
+        }
+
+        static char[] EncryptSong(char[] song)
+        {
+            return song;
+        }
+
+        static bool CheckSongIsAvaible(char[] song)
+        {
+            for (int i = 0; i < song.Length; i++)
+            {
+                if (song[0] >= 'A' && song[0] <= 'Z')
+                {
+                    continue;
+                }
+
+                if (song[i] == ' ')
+                {
+                    continue;
+
+                }
+                if (song[0] < 'A' || song[0] > 'Z')
+                {
+
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        static bool CheckArtistIsAvaible(char[] artist)
+        {
+            if (artist[0] >= 'A' && artist[0] <= 'Z')
+            {
+                for (int i = 1; i < artist.Length; i++)
+                {
+                    if (artist[i] == ' ' || artist[i] == '\'')
+                    {
+                        continue;
+                    }
+                    if (artist[i] < 'a' || artist[i] > 'z')
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ FINAL EXAMS - Compilation/03. The Isle of Man TT Race/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+
+namespace _03._The_Isle_of_Man_TT_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"^(?<sing>\W)(\w+)\k'sing'=(\d*)!!(.*)";
+            var regex = new Regex(pattern);
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var match = regex.Match(input);
+                if (match.Success)
+                {
+                    var name = match.Groups[1].Value;
+                    var length = int.Parse(match.Groups[2].Value);
+                    var text = match.Groups[3].Value;
+                    if (text.Length == length)
+                    {
+                        Console.WriteLine($"Coordinates found! {name} -> {Encrypt(text.ToCharArray())}");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+
+
+            }
+        }
+
+        static string Encrypt(char[] text)
+        {
+
+            var newText = string.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                text[i] += (char)text.Length;
+                newText += text[i];
+            }
+            return newText;
+        }
+    }
+}
