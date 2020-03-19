cf5bb66365b37bdc213d244dfa73e577495e588f
tonkatawe <vlahov.anton@gmail.com>
2020-03-19 23:30:51 +0200

    C# Fundamentals

---
+++ FINAL EXAMS - Compilation/01. Arriving in Kathmandu/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Security;
+using System.Text.RegularExpressions;
+
+namespace _01._Arriving_in_Kathmandu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"^([A-Za-z0-9!@#$?]+)=(\d+)<<(.+)";
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "Last note")
+                {
+                    break;
+                }
+
+                var regex = new Regex(pattern);
+                var match = regex.Match(command);
+                if (match.Success)
+                {
+                    var name = match.Groups[1].Value;
+                    var length = int.Parse(match.Groups[2].Value);
+                    var code = match.Groups[3].Value;
+                    if (length == code.Length)
+                    {
+                        Console.WriteLine($"Coordinates found! {EncryptedName(name.ToCharArray())} -> {code}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+            }
+
+        }
+
+        static string EncryptedName(char[] name)
+        {
+            var peak = string.Empty;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (char.IsLetterOrDigit(name[i]))
+                {
+                    peak += name[i];
+                }
+            }
+            return peak;
+        }
+    }
+}
---
+++ FINAL EXAMS - Compilation/02. Practice Sessions/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Practice_Sessions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> roadsAndRacers = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+
+                var tokens = command.Split("->", StringSplitOptions.RemoveEmptyEntries);
+                var instruction = tokens[0];
+                if (instruction == "Add")
+                {
+                    var road = tokens[1];
+                    var racer = tokens[2];
+                    if (!roadsAndRacers.ContainsKey(road))
+                    {
+                        roadsAndRacers.Add(road, new List<string>());
+                        roadsAndRacers[road].Add(racer);
+                    }
+                    //here is possible to check for exist user
+                    else
+                    {
+                        roadsAndRacers[road].Add(racer);
+                    }
+                }
+                else if (instruction == "Move")
+                {
+                    var currRoad = tokens[1];
+                    var racer = tokens[2];
+                    var nextRoad = tokens[3];
+                    if (roadsAndRacers.ContainsKey(currRoad) && roadsAndRacers.ContainsKey(nextRoad) && roadsAndRacers[currRoad].Contains(racer))
+                    {
+                        roadsAndRacers[currRoad].Remove(racer);
+                        roadsAndRacers[nextRoad].Add(racer);
+                    }
+                }
+                else if (instruction =="Close")
+                {
+                    var road = tokens[1];
+                    if (roadsAndRacers.ContainsKey(road))
+                    {
+                        roadsAndRacers.Remove(road);
+                    }
+                }
+            }
+
+            Console.WriteLine("Practice sessions:");
+            foreach (var road in roadsAndRacers.OrderByDescending(x=> x.Value.Count).ThenBy(x =>x.Key))
+            {
+                Console.WriteLine($"{road.Key}");
+                foreach (var racer in road.Value)
+                {
+                    Console.WriteLine($"++{racer}");
+                }
+            }
+        }
+    }
+}
