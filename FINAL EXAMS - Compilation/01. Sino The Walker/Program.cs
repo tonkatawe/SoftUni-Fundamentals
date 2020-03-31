7e054f9cb0918c72f3b41bd87d8412b4bd267015
tonkatawe <vlahov.anton@gmail.com>
2020-03-31 15:45:18 +0300

    C# Fundamentals

---
+++ FINAL EXAMS - Compilation/01. Sino The Walker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using Microsoft.VisualBasic;
+
+namespace _01._Sino_The_Walker
+{
+    class Program
+    {
+        //I have only 80/100 by judge but I don't know why ?! I tried to make if checks for steps and seconds but it doesn't matter
+        static void Main(string[] args)
+        {
+            //input
+            var inputTime = Console.ReadLine();
+            var steps = int.Parse(Console.ReadLine());
+            var seconds = int.Parse(Console.ReadLine());
+            //problem solve
+
+            var secondsPerStep = steps * seconds;
+
+            string dataFromat = "HH:mm:ss";
+            DateTime time = DateTime.ParseExact(inputTime, dataFromat, null);
+            string result = time.AddSeconds(secondsPerStep).ToString(dataFromat);
+            Console.WriteLine($"Time Arrival: {result}");
+
+        }
+    }
+}
---
+++ FINAL EXAMS - Compilation/02. SoftUni Karaoke/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._SoftUni_Karaoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var participants = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+            var songs = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+            var result = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "dawn")
+                {
+                    break;
+                }
+
+                var tokens = command.Split(", ");
+                var name = tokens[0];
+                var song = tokens[1];
+                var award = tokens[2];
+                if (participants.Contains(name) && songs.Contains(song))
+                {
+                    if (!result.ContainsKey(name))
+                    {
+                        result[name] = new List<string>();
+                    }
+                    if (result.ContainsKey(name) && !result[name].Contains(award))
+                    {
+                        result[name].Add(award);
+                    }
+                }
+            }
+
+            if (result.Count == 0)
+            {
+                Console.WriteLine("No awards");
+
+            }
+            else
+            {
+                foreach (var singer in result.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{singer.Key}: {singer.Value.Count} awards");
+                    foreach (var award in singer.Value.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"--{award}");
+                    }
+                }
+            }
+        }
+    }
+}
